b3dcb72b97c47456ff9182755a93cb28eef1afeb
Omotosho Olusegun <99738261+Aypaul95@users.noreply.github.com>
2026-01-20 12:49:38 -0800

    Update schema.sql

--- sql-projects/e-commerce_customer_behavior_analysis/schema.sql
+++ sql-projects/e-commerce_customer_behavior_analysis/schema.sql
@@ -1,1 +1,110 @@
+USE [e-commerce_customer_behavior_analysis]
+GO
+/****** Object:  Table [dbo].[customers]    Script Date: 1/20/2026 1:28:45 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[customers](
+	[customer_id] [int] NOT NULL,
+	[first_name] [varchar](50) NOT NULL,
+	[last_name] [varchar](50) NOT NULL,
+	[email] [varchar](100) NOT NULL,
+	[gender] [varchar](10) NOT NULL,
+	[signup_date] [date] NOT NULL,
+	[country] [varchar](50) NOT NULL,
+	[city] [varchar](50) NOT NULL,
+	[customer_segment] [varchar](30) NOT NULL,
+ CONSTRAINT [PK_customers] PRIMARY KEY CLUSTERED 
+(
+	[customer_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[order_items]    Script Date: 1/20/2026 1:28:45 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[order_items](
+	[order_item_id] [int] NOT NULL,
+	[order_id] [int] NOT NULL,
+	[product_id] [int] NOT NULL,
+	[quantity] [int] NOT NULL,
+	[unit_price] [decimal](10, 2) NOT NULL,
+	[discount] [decimal](5, 2) NOT NULL,
+	[total_price] [decimal](10, 2) NOT NULL,
+ CONSTRAINT [PK_order_items] PRIMARY KEY CLUSTERED 
+(
+	[order_item_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[orders]    Script Date: 1/20/2026 1:28:45 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[orders](
+	[order_id] [int] NOT NULL,
+	[customer_id] [int] NOT NULL,
+	[order_date] [date] NOT NULL,
+	[order_status] [varchar](20) NOT NULL,
+	[total_amount] [decimal](10, 2) NOT NULL,
+	[shipping_cost] [decimal](10, 2) NOT NULL,
+	[payment_id] [int] NOT NULL,
+ CONSTRAINT [PK_orders] PRIMARY KEY CLUSTERED 
+(
+	[order_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[payments]    Script Date: 1/20/2026 1:28:45 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[payments](
+	[payment_id] [int] NOT NULL,
+	[payment_date] [date] NOT NULL,
+	[payment_method] [varchar](30) NOT NULL,
+	[payment_status] [varchar](20) NOT NULL,
+	[payment_amount] [decimal](10, 2) NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[products]    Script Date: 1/20/2026 1:28:45 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[products](
+	[product_id] [int] NOT NULL,
+	[product_name] [varchar](100) NOT NULL,
+	[category] [varchar](50) NOT NULL,
+	[brand] [varchar](50) NOT NULL,
+	[cost_price] [decimal](10, 2) NOT NULL,
+	[selling_price] [decimal](10, 2) NOT NULL,
+	[stock_quantity] [int] NOT NULL,
+	[launch_date] [date] NOT NULL,
+ CONSTRAINT [PK_products] PRIMARY KEY CLUSTERED 
+(
+	[product_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[order_items]  WITH CHECK ADD  CONSTRAINT [FK_order_items_orders] FOREIGN KEY([order_id])
+REFERENCES [dbo].[orders] ([order_id])
+GO
+ALTER TABLE [dbo].[order_items] CHECK CONSTRAINT [FK_order_items_orders]
+GO
+ALTER TABLE [dbo].[order_items]  WITH CHECK ADD  CONSTRAINT [FK_order_items_products] FOREIGN KEY([product_id])
+REFERENCES [dbo].[products] ([product_id])
+GO
+ALTER TABLE [dbo].[order_items] CHECK CONSTRAINT [FK_order_items_products]
+GO
+ALTER TABLE [dbo].[orders]  WITH CHECK ADD  CONSTRAINT [FK_orders_cutomers] FOREIGN KEY([customer_id])
+REFERENCES [dbo].[customers] ([customer_id])
+GO
+ALTER TABLE [dbo].[orders] CHECK CONSTRAINT [FK_orders_cutomers]
+GO
 
